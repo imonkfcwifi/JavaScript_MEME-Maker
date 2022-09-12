c338cd8357b9df47e73c1127d9930f1af67a6c2c
Roh <nohyungkorea@gmail.com>
2022-09-12 23:52:50 +0900

    add some good colors

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const canvas = document.querySelector("canvas");
-
+const lineWidth = document.getElementById("line-width");
 const context = canvas.getContext("2d");
-
+const color = document.getElementById("color");
+const colorOptions = Array.from(document.querySelectorAll(".color-option")
+);
 canvas.width = 800;
 canvas.height = 800;
 
-context.lineWidth = 2;
+context.lineWidth = lineWidth.value;
 let isPainting = false;
 
-const colors = [
-    "#ff3838",
-    "#ffb8b8",
-    "#c56cf0",
-    "#ff9f1a",
-    "#fff200",
-    "#32ff7e",
-    "#7efff5",
-    "#18dcff",
-    "#7d5fff"
-];
+function onColorClick(event) {
+    context.strokeStyle = event.target.dataset.color;
+    context.fillStyle = event.target.dataset.color;
+    color.value = event.target.dataset.color;
+
+}
 
 function onMove(event) {
     if (isPainting) {
@@ -26,6 +23,7 @@ function onMove(event) {
         context.stroke();
         return;
     }
+    context.beginPath();
     context.moveTo(event.offsetX, event.offsetY);
 
 }
@@ -36,8 +34,21 @@ function onmousedown() {
 function cancle() {
     isPainting = false;
 }
+
+function onlineWidthChange(event) {
+    context.lineWidth = event.target.value;
+}
+function oncolorchange(event) {
+    context.strokeStyle = event.target.value;
+    context.fillStyle = event.target.value;
+}
+
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onmousedown);
 canvas.addEventListener("mouseup", cancle);
 canvas.addEventListener("mouseleave", cancle);
+lineWidth.addEventListener("change", onlineWidthChange);
+color.addEventListener("change", oncolorchange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 // 항상 마지막에 채워줘야 함.
